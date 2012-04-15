1642e0cc2a0374a6ab1c5069c5acfd165d612843
Douglas Squirrel <ds@douglassquirrel.com>
2012-04-15 20:26:57 +0100

    fix install instructions

--- deploy-vm.sh
+++ deploy-vm.sh
@@ -1,10 +1,8 @@
-ssh -t $WB_USER@whooshingby-vm "export WB_ROOT=$WB_ROOT
-export HOST=$HOST
-sudo apt-get -y install unzip
+ssh -t $WB_USER@whooshingby-vm "sudo apt-get -y install unzip
 sudo mkdir $WB_ROOT
 sudo chown $WB_USER $WB_ROOT
 wget -O $WB_ROOT/whooshingby.zip https://github.com/douglassquirrel/whooshingby/zipball/master
 unzip $WB_ROOT/whooshingby.zip -d $WB_ROOT
 mv $WB_ROOT/*whooshingby*/* $WB_ROOT
 rm -rf $WB_ROOT/whooshingby.zip $WB_ROOT/*-whooshingby-*
-sudo $WB_ROOT/install/install.sh"
+sudo WB_ROOT=$WB_ROOT HOST=$HOST $WB_ROOT/install/install.sh"
