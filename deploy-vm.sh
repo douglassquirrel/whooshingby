46ace70c83d1d2cb6d7139ab7a42577725f44193
Douglas Squirrel <ds@douglassquirrel.com>
2012-04-15 19:57:29 +0100

    clean up install

---
+++ deploy-vm.sh
@@ -0,0 +1,8 @@
+ssh -t $WB_USER@whooshingby-vm "sudo apt-get -y install unzip
+sudo mkdir $WB_ROOT
+sudo chown $WB_USER $WB_ROOT
+wget -O $WB_ROOT/whooshingby.zip https://github.com/douglassquirrel/whooshingby/zipball/master
+unzip $WB_ROOT/whooshingby.zip -d $WB_ROOT
+mv $WB_ROOT/*whooshingby*/* $WB_ROOT
+rm -rf $WB_ROOT/whooshingby.zip $WB_ROOT/*-whooshingby-*
+sudo $WB_ROOT/install/install.sh"
