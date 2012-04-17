5d3689c1032d764c0c37b32ffcca33cb1605a43c
Douglas Squirrel <ds@douglassquirrel.com>
2012-04-17 17:05:05 +0100

    add behat and sqlite

--- install/install.sh
+++ install/install.sh
@@ -14,15 +14,22 @@ log "Installing Apache."
   apt-get -y install apache2
 log "Installing PHP."
   apt-get -y install libapache2-mod-php5
-log "Installing PHPUnit." 
+log "Installing pear."
   apt-get -y install php-pear
   apt-get -y upgrade php-pear
+log "Installing PHPUnit." 
   pear channel-discover pear.phpunit.de
   pear channel-discover pear.symfony-project.com
   pear channel-discover components.ez.no
   pear update-channels
   pear upgrade-all
   pear install --alldeps phpunit/PHPUnit
+log "Installing behat."
+  pear channel-discover pear.symfony.com
+  pear channel-discover pear.behat.org
+  pear install --alldeps behat/behat
+log "Installing sqlite"
+  apt-get -y install sqlite3
 log "Installing whooshingby." 
   sed -e "s:@WB_ROOT:$WB_ROOT:g" -e "s:@HOST:$HOST:g" $WB_ROOT/install/whooshingby.conf | sudo tee /etc/apache2/sites-available/whooshingby.conf
   a2ensite whooshingby.conf
