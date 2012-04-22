0958cdb4a4bff88a41a68cc2f089e9785a974154
Douglas Squirrel <ds@douglassquirrel.com>
2012-04-22 22:31:22 +0100

    check variables set in deploy-vm.sh

--- deploy-vm.sh
+++ deploy-vm.sh
@@ -1,7 +1,13 @@
-ssh -t $WB_USER@whooshingby-vm "sudo apt-get -y update
+echo "VM_USER=${VM_USER}, VM_NAME=${VM_NAME}, WB_ROOT=${WB_ROOT}, HOST=${HOST}"
+if [ -z "${VM_USER}" ] || [ -z "${VM_NAME}" ] || [ -z "${WB_ROOT}" ] || [ -z "${HOST}" ] ; then
+  echo "Need to set one of the above variables"
+  exit;
+fi
+ 
+ssh -t $VM_USER@$VM_NAME "sudo apt-get -y update
 sudo apt-get -y install unzip
 sudo mkdir $WB_ROOT
-sudo chown $WB_USER $WB_ROOT
+sudo chown $VM_USER $WB_ROOT
 wget -O $WB_ROOT/whooshingby.zip https://github.com/douglassquirrel/whooshingby/zipball/master
 unzip $WB_ROOT/whooshingby.zip -d $WB_ROOT
 mv $WB_ROOT/*whooshingby*/* $WB_ROOT
