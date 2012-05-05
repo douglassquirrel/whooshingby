599a4289e306aa86083e22215b2c6fa722cca697
Douglas Squirrel <ds@douglassquirrel.com>
2012-05-05 19:52:09 +0100

    more fact storage

--- classes/FactStore.php
+++ classes/FactStore.php
@@ -1,11 +1,19 @@
 <?php
 class FactStore {
-    function register_type($type) {}
-    function add($type, $text) {
-        return 1;
+    function __construct() {
+        $this->repo = array();
     }
+
+    function register_type($type) {}
+
+    function add($type, $fact_array) {
+        $fact_array["__Type"] = $type;
+        $this->repo[] = $fact_array;
+	return count($this->repo);
+    }
+
     function get_geq_id($id) {
-        return array(array("foo"));
+        return $this->repo;
     }
 }
 ?>
