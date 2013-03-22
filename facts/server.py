f9592589a58ade3701ee295518da2e60dd598e6d
Douglas Squirrel <ds@douglassquirrel.com>
2013-03-22 18:42:11 +0000

    record type and timestamp

--- facts/server.py
+++ facts/server.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from cgi import parse_header, parse_multipart, parse_qs
+from json import dumps
 from os import environ, path
 from sys import exit
-from cgi import parse_header, parse_multipart, parse_qs
+from time import time
 
 PORT           = int(environ.get('PORT') or None)
 FACTS_LOCATION = environ.get('FACTS_LOCATION')
@@ -10,6 +12,12 @@ if PORT is None or FACTS_LOCATION is None:
     print "Need to set PORT and FACTS_LOCATION environment variables"
     exit(1)
 
+def save_fact(fact_type, content):
+    tstamp = int(time())
+    name = '.'.join([fact_type, str(tstamp), 'fact'])
+    with open(path.join(FACTS_LOCATION, name), 'w') as fact_file:
+        fact_file.write(content)
+    
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
         ctype, pdict = parse_header(self.headers.getheader('content-type'))
@@ -21,11 +29,10 @@ class handler(BaseHTTPRequestHandler):
         else:
             postvars = {}
         
-        name    = (postvars.get('name')    or [None])[0]
-        content = (postvars.get('content') or [None])[0]
-        if name and content:
-            with open(path.join(FACTS_LOCATION, name), 'w') as fact_file:
-                fact_file.write(content)
+        fact_type = (postvars.get('type')    or [None])[0]
+        content   = (postvars.get('content') or [None])[0]
+        if fact_type and content:
+            save_fact(fact_type, content)
             response_code = 200
         else:
             response_code = 400
@@ -36,8 +43,6 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
         return
 
-# Start fact
+save_fact('service-started', dumps({'name':'facts', 'port':PORT}))
 server = HTTPServer(('', PORT), handler)
 server.serve_forever()
-
-    
