037c96325551d1524f1f17d6df49ff63742834bf
Douglas Squirrel <ds@douglassquirrel.com>
2013-03-23 11:50:25 +0000

    Record server start facts

--- task-submit-html/skeleton/server.py
+++ task-submit-html/skeleton/server.py
@@ -2,12 +2,19 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from os import environ
 from sys import exit
+from httplib2 import Http
+from urllib import urlencode
+from json import dumps
 
 CACHE_MAX_AGE = environ.get('CACHE_MAX_AGE')
 PORT          = int(environ.get('PORT') or None)
+FACT_URL      = environ.get('FACT_URL')
 if CACHE_MAX_AGE is None or PORT is None:
-    # Failed to start fact
-    print "Need to set both CACHE_MAX_AGE and PORT environment variables"
+    content = dumps({'name':'task-submit-html-skeleton', 'reason':'missing environment variables'})
+    request_body = urlencode({'type':'service-failed-to-start', 'content':content})
+    Http().request(FACT_URL, "POST", request_body, headers={'content-type':'application/x-www-form-urlencoded'})
+if CACHE_MAX_AGE is None or PORT is None or FACT_URL is None:    
+    print "Need to set CACHE_MAX_AGE, PORT and FACT_URL environment variables"
     exit(1)
 with open('skeleton.htmlfragment') as f: SKELETON = f.read()
 
@@ -21,7 +28,10 @@ class handler(BaseHTTPRequestHandler):
         self.wfile.write(SKELETON)
         return
 
-# Start fact
+content = dumps({'name':'task-submit-html-skeleton', 'port':PORT})
+request_body = urlencode({'type':'service-started', 'content':content})
+Http().request(FACT_URL, "POST", request_body, headers={'content-type':'application/x-www-form-urlencoded'})
+
 server = HTTPServer(('', PORT), handler)
 server.serve_forever()
 
