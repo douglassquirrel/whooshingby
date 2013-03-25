60a524e9b96848ad0baceb1dd776b7fefe40e79e
Douglas Squirrel <ds@douglassquirrel.com>
2013-03-25 15:39:19 +0000

    create fact when task complted

--- task-submit-html/behaviour/completed_task.js
+++ task-submit-html/behaviour/completed_task.js
@@ -1,3 +1,8 @@
 function completed_task() {
-    alert("Completed task!");
+    xmlhttp = new XMLHttpRequest();
+    var url = 'FACT_URL' + '/completed-task';
+    var content = '{"client_address":"CLIENT_ADDRESS"}'
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send(content);
 }
---
+++ task-submit-html/behaviour/server.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from httplib2 import Http
+from json import dumps
+from random import randrange
+from urllib import urlencode
+
+def find_facts_service():
+    for port in range(2000, 3000):
+        url = 'http://localhost:%d' % port
+        try:
+            resp, content = Http().request(url)
+            if resp.status == 200 and 'Kropotkin' in content:
+                return url
+        except IOError:
+            pass
+    raise Exception('Cannot locate facts service')
+
+CACHE_MAX_AGE = 5
+
+FACT_URL      = find_facts_service()
+print "Using facts service at %s" % FACT_URL
+
+PORT = randrange(2000, 3000)
+print "Listening on port %d" % PORT
+
+with open('completed_task.js') as f: BEHAVIOUR = f.read()
+BEHAVIOUR = BEHAVIOUR.replace('FACT_URL', FACT_URL)
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        behaviour = BEHAVIOUR.replace('CLIENT_ADDRESS', self.client_address[0])
+        self.send_response(200)
+        self.send_header('Cache-Control',  'max-age=%d' % CACHE_MAX_AGE)
+        self.send_header('Content-Length', len(behaviour))
+        self.send_header('Content-Type',   'text/html')
+        self.end_headers()
+        self.wfile.write(behaviour)
+        return
+
+content = dumps({'name':'task-submit-html-behaviour', 'port':PORT})
+Http().request(FACT_URL + "/service-started", "POST", content, headers={'content-type':'application/x-www-form-urlencoded'})
+
+server = HTTPServer(('', PORT), handler)
+server.serve_forever()
+
+    
