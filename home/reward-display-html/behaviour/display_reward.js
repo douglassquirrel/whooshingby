c18b354ce1b49e00caf8789d6f059f17877f29bb
Douglas Squirrel <ds@douglassquirrel.com>
2013-03-27 18:24:16 +0000

    Display reward messages

--- home/reward-display-html/behaviour/display_reward.js
+++ home/reward-display-html/behaviour/display_reward.js
@@ -1,30 +1,41 @@
-function display_reward()
-{
+rewards = [];
+
+function check_reward() {
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4) {
+      server_rewards = JSON.parse(xmlhttp.responseText);
+      server_rewards.sort(compare_rewards);
+        if (server_rewards.length > rewards.length) {
+          latest_reward = server_rewards.slice(-1)[0]
+          display_reward(latest_reward['name']);
+        }
+      rewards = server_rewards;
+    }
+  }
+  xmlhttp.open("GET", FACT_URL + '/reward', true);
+  xmlhttp.send();
 }
 
 function compare_rewards(r, s) {
-    if      (r['time'] != s['time']) { return r['time'] - s['time']; }
-
-    if      (r['name'] < s['name'])  { return -1; }
-    else if (r['name'] > s['name'])  { return 1;  }
-    else                             { return 0; }
+  if      (r['time'] != s['time']) { return r['time'] - s['time']; }
+  else if (r['name'] < s['name'])  { return -1; }
+  else if (r['name'] > s['name'])  { return 1;  }
+  else                             { return 0; }
 }
 
-rewards = [];
-function check_reward() {
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState == 4) {
-        server_rewards = JSON.parse(xmlhttp.responseText);
-        server_rewards.sort(compare_rewards);
-          if (server_rewards.length > rewards.length) {
-            alert(JSON.stringify(server_rewards.slice(-1)[0]));
-          }
-        rewards = server_rewards;
-      }
-    }
-    xmlhttp.open("GET", FACT_URL + '/reward', true);
-    xmlhttp.send();
+function display_reward(name)
+{
+  reward_elts = document.querySelectorAll('*[data-reward]');
+  for (i=0; i<reward_elts.length; i++) {
+    reward_elts[i].style.display = 'none';
+  }
+  chosen_element = reward_elts[Math.floor(Math.random() * reward_elts.length)];
+  chosen_element.style.display = 'block';
+  name_elts = chosen_element.querySelectorAll('*[data-reward-name]');
+  for (i=0; i<name_elts.length; i++) {
+    name_elts[i].innerHTML = name;
+  }
 }
 
-setInterval(check_reward, 10000);
+setInterval(check_reward, 1000);
