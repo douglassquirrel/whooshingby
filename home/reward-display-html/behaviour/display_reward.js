12f4883d427a7cf4473e2072cf373bd815dd93e0
Douglas Squirrel <ds@douglassquirrel.com>
2013-03-27 13:35:36 +0000

    Detect new rewards correctly

--- home/reward-display-html/behaviour/display_reward.js
+++ home/reward-display-html/behaviour/display_reward.js
@@ -2,12 +2,25 @@ function display_reward()
 {
 }
 
+function compare_rewards(r, s) {
+    if      (r['time'] != s['time']) { return r['time'] - s['time']; }
+
+    if      (r['name'] < s['name'])  { return -1; }
+    else if (r['name'] > s['name'])  { return 1;  }
+    else                             { return 0; }
+}
+
+rewards = [];
 function check_reward() {
     xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
       if (xmlhttp.readyState == 4) {
-        new_rewards = JSON.parse(xmlhttp.responseText);
-          alert(JSON.stringify(new_rewards));
+        server_rewards = JSON.parse(xmlhttp.responseText);
+        server_rewards.sort(compare_rewards);
+          if (server_rewards.length > rewards.length) {
+            alert(JSON.stringify(server_rewards.slice(-1)[0]));
+          }
+        rewards = server_rewards;
       }
     }
     xmlhttp.open("GET", FACT_URL + '/reward', true);
