3d37e1a5113377f277a38e232a89bd66d8f29c35
Douglas Squirrel <douglas.squirrel@secretsales.com>
2013-04-15 18:30:37 +0100

    Update to deploy verb

--- run.py
+++ run.py
@@ -3,9 +3,14 @@ from kropotkin import store_fact
 from json import dumps
 from os import environ
 from os.path import abspath
+from sys import exit
 from time import time
 
-KROPOTKIN_URL = environ['KROPOTKIN_URL']
+try:
+    KROPOTKIN_URL = environ['KROPOTKIN_URL']
+except KeyError:
+    print "Must set environment variable KROPOTKIN_URL"
+    exit(1)
 
 content = dumps({'time': int(time()), 'directory': abspath('components')})
-store_fact(KROPOTKIN_URL, 'start-component', content)
+store_fact(KROPOTKIN_URL, 'deploy', content)
