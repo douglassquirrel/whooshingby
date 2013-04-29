61cbcaeebfe2ce8c8393e76ddab9badbc3690889
Douglas Squirrel <douglas.squirrel@secretsales.com>
2013-04-29 22:55:18 +0100

    use javascript library

--- components/completed_task/completed_task.js
+++ components/completed_task/completed_task.js
@@ -1,9 +1,4 @@
-POST_MIME_TYPE = "application/x-www-form-urlencoded"
 function completed_task() {
-    task_xmlhttp = new XMLHttpRequest();
-    var url = '/factspace/kropotkin/completed-task';
-    var content = '{"client_address":"' + '0.0.0.0' + '"}'
-    task_xmlhttp.open("POST", url, true);
-    task_xmlhttp.setRequestHeader("Content-type", POST_MIME_TYPE);
-    task_xmlhttp.send(content);
+    var content = {'client_address': '0.0.0.0'}
+    store_fact('kropotkin', 'completed-task', content)
 }
