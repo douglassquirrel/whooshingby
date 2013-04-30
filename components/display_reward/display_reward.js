45f6d4d03d736c8e975d96e54b8e1eabd21d40a0
Douglas Squirrel <douglas.squirrel@secretsales.com>
2013-04-30 20:14:10 +0100

    Use new JS library

--- components/display_reward/display_reward.js
+++ components/display_reward/display_reward.js
@@ -1,41 +1,27 @@
 function check_reward() {
-  reward_xmlhttp = new XMLHttpRequest();
-  reward_xmlhttp.onreadystatechange = function() {
-    if (reward_xmlhttp.readyState != 4) { return; }
-
-    if (reward_xmlhttp.responseText.length > 0) {
-      server_rewards = JSON.parse(reward_xmlhttp.responseText);
-      if (server_rewards.length > 0) {
-        display_reward(server_rewards[0]['name']);
-      }
-    }
-  }
-
-  /* Need to add a unique id here */
-  query_string = 'kropotkin_criteria=stamp-display_reward.3141,result-oldest';
-  url = '/factspace/kropotkin/reward?' + query_string;
-
-  reward_xmlhttp.open("GET", url, true);
-  reward_xmlhttp.send();
+    get_oldest_fact_and_stamp('kropotkin', 'reward', [],
+                              'display_reward.3141', display_reward);
 }
 
-function display_reward(name)
+function display_reward(fact)
 {
-  reward_elts = document.querySelectorAll('*[data-reward]');
-  hidden_elts = []
-  for (i=0; i<reward_elts.length; i++) {
-      if (reward_elts[i].style.display != 'none') {
-        reward_elts[i].style.display = 'none';
-      } else {
-        hidden_elts.push(reward_elts[i]);
-      }
-  }
-  chosen_element = hidden_elts[Math.floor(Math.random() * hidden_elts.length)];
-  chosen_element.style.display = 'block';
-  name_elts = chosen_element.querySelectorAll('*[data-reward-name]');
-  for (i=0; i<name_elts.length; i++) {
-    name_elts[i].innerHTML = name;
-  }
+    name = fact['name'];
+    reward_elts = document.querySelectorAll('*[data-reward]');
+    hidden_elts = [];
+    for (i=0; i<reward_elts.length; i++) {
+        if (reward_elts[i].style.display != 'none') {
+            reward_elts[i].style.display = 'none';
+        } else {
+            hidden_elts.push(reward_elts[i]);
+        }
+    }
+    r = Math.random();
+    chosen_element = hidden_elts[Math.floor(r * hidden_elts.length)];
+    chosen_element.style.display = 'block';
+    name_elts = chosen_element.querySelectorAll('*[data-reward-name]');
+    for (i=0; i<name_elts.length; i++) {
+        name_elts[i].innerHTML = name;
+    }
 }
 
 setInterval(check_reward, 1000);
